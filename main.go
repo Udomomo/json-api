e58333c7716b3edb2e82b36b563da73c498e5424
Udomomo <batzuma@yahoo.co.jp>
2018-09-13 23:57:12 +0900

    ハンドラを追加

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoID}", TodoShow)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+//Index ハンドラ
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Wellcome!")
+}
+
+//TodoIndex ハンドラ
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Todo Index!")
+}
+
+//TodoShow ハンドラ
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoID"]
+	fmt.Fprintln(w, "Todo show:", todoID)
 }
