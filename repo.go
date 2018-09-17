25246d0d4a9f411ee1615a39e39fa773f1456176
Udomomo <batzuma@yahoo.co.jp>
2018-09-17 23:13:59 +0900

    簡易DBを作成

---
+++ repo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var currentID int
+
+var todos Todos
+
+// Give us some seed data
+func init() {
+	RepoCreateTodo(Todo{Name: "Write presentation"})
+	RepoCreateTodo(Todo{Name: "Host meetup"})
+}
+
+// RepoFindTodo 項目を検索
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.ID == id {
+			return t
+		}
+	}
+	// return empty Todo if not found
+	return Todo{}
+}
+
+// RepoCreateTodo 項目を作成
+func RepoCreateTodo(t Todo) Todo {
+	currentID++
+	t.ID = currentID
+	todos = append(todos, t)
+	return t
+}
+
+// RepoDestroyTodo 項目を削除
+func RepoDestroyTodo(id int) error {
+	for i, t := range todos {
+		if t.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+}
