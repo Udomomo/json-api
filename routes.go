494c3c25c53967aef88107d651e72439856ce29c
Udomomo <batzuma@yahoo.co.jp>
2018-09-17 17:22:04 +0900

    routeを2つのファイルに分割

---
+++ routes.go
@@ -0,0 +1,37 @@
+package main
+
+import "net/http"
+
+//ルーターを作るところだけ切り出したもの
+
+//Route ルーターとして必要な情報をstructにまとめたもの
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+//Routes ルーターの配列
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/todos",
+		TodoIndex,
+	},
+	Route{
+		"TodoShow",
+		"GET",
+		"/todos/{todoId}",
+		TodoShow,
+	},
+}
